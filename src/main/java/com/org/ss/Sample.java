154d7fd813986f38178d8aaee34d8e961b2d8727
=venkatesh <=pachakolla.venkatesh@s-force.org>
2018-11-01 11:49:10 +0530

    added sucess

--- src/main/java/com/org/ss/Sample.java
+++ src/main/java/com/org/ss/Sample.java
@@ -5,6 +5,11 @@ public class Sample
 	public void m1(String data)
 	{
 		System.out.println("this is your Data:"+data);
+		
+		if(data.equals("venky"))
+		{
+			System.out.println("welcome");
+		}
 	}
 
 	public static void main(String[] args)
