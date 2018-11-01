5c5110639924ed714608a6288b11ff1b474c4b13
=venkatesh <=pachakolla.venkatesh@s-force.org>
2018-11-01 11:40:16 +0530

    added git -demo local repo

---
+++ src/main/java/com/org/ss/Sample.java
@@ -0,0 +1,16 @@
+package com.org.ss;
+
+public class Sample
+{
+	public void m1(String data)
+	{
+		System.out.println("this is your Data:"+data);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+
+	}
+
+}
